0cf4e0f265af57010fdc9cb17454992658fd11a9
Ayusha0148 <ayusha0148.be20@chitkara.edu.in>
2022-08-12 23:40:35 +0530

    Project-Final

--- project.js
+++ project.js
@@ -44,48 +44,43 @@ function toggleMode(){
     count+=1;
 }
 btn.addEventListener("click",toggleMode)
-document.querySelector('#button').addEventListener('click', () => {
-    // API call
-    const xhr = new XMLHttpRequest()
-    const value = document.querySelector('#search').value
-
-    const url = `https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09&q=${value}&maxResults=50`
-
-    xhr.open('GET', url, true)
-
-    xhr.onreadystatechange = () => {
-        if(xhr.status === 200 && xhr.readyState === 4) {
-            let response = JSON.parse(xhr.responseText)
-
-            let list = document.getElementById('list1');
-            let output = ''
-            for(key in response) {
-                output += `
-                
-                <li>Name: ${response[key].name}</li>
-                <li>Username: ${response[key].username}</li>
-                <li>Email-id: ${response[key].email}</li>
-                <li>Street: ${response[key].address.street}</li>
-                <li>Suite: ${response[key].address.suite}</li>
-                <li>City: ${response[key].address.city}</li>
-                <li>Zipcode: ${response[key].address.zipcode}</li>
-                <li>Latitude: ${response[key].address.geo.lat}</li>
-                <li>Longitude: ${response[key].address.geo.lng}</li>
-                <li>Phone-number: ${response[key].phone}</li>
-                <li>Website: ${response[key].website}</li>
-                <li>Company-Name: ${response[key].company.name}</li>
-                <li>Company-CatchPhrase: ${response[key].company.catchPhrase}</li>
-                <li>Company-BS: ${response[key].company.bs}</li>
-                
-            `
-            }
-            list.innerHTML=output;
-        }
-    }
-
-    xhr.send()
-})
-document.getElementById('my-form').addEventListener("submit",function(e){
-    e.preventDefault()
-})
-
+window.onload = function () {
+    var x = new XMLHttpRequest();
+    x.onload = function () {
+      var json = JSON.parse(x.response);
+      console.log(json);
+      let output='';
+      for (let key = 0; key < json.length; key++) {
+        let ele = document.createElement("div");
+        let output='';
+        ele.style.cssText =
+          "background-color:rgb(208, 211, 212 );width:30vw;height:30vh;border:2px solid black;border-radius:10px 10px 10px 10px;margin-top:0.5rem";
+          output += `
+            <ul id="list1">
+                <li>Name: ${json[key].name}</li>
+                <li>Username: ${json[key].username}</li>
+                <li>Email-id: ${json[key].email}</li>
+                <li>Street: ${json[key].address.street}</li>
+                <li>Suite: ${json[key].address.suite}</li>
+                <li>City: ${json[key].address.city}</li>
+                <li>Zipcode: ${json[key].address.zipcode}</li>
+                <li>Latitude: ${json[key].address.geo.lat}</li>
+                <li>Longitude: ${json[key].address.geo.lng}</li>
+                <li>Phone-number: ${json[key].phone}</li>
+                <li>Website: ${json[key].website}</li>
+                <li>Company-Name: ${json[key].company.name}</li>
+                <li>Company-CatchPhrase: ${json[key].company.catchPhrase}</li>
+                <li>Company-BS: ${json[key].company.bs}</li>
+            </ul>
+      `
+        ele.innerHTML=output;
+        document.getElementById("data").appendChild(ele);
+      }
+      console.log(typeof json[2].body);
+    };
+    x.open(
+      "get",
+      "https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-11"
+    );
+    x.send();
+  };
